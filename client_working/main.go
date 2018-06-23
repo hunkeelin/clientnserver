eb801b70763b8de016af3b68409d71024c886da0
bgops <bgops@test3.klin-pro.com>
2018-06-22 21:21:08 -0700

    added req package

--- client_working/main.go
+++ client_working/main.go
@@ -3,7 +3,6 @@ package main
 import ()
 
 func main() {
-	//	sendcsrv2("master.pem", "https://test3.klin-pro.com:2018")
-	testreq("master.pem", "https://test3.klin-pro.com:2018")
-	testreqv2("master.pem", "https://test3.klin-pro.com:2018")
+	testreq("master.pem", "http://test3.klin-pro.com:2018")
+	sendfile("master.pem", "https://test3.klin-pro.com:2018")
 }
